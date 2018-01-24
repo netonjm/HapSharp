dd8959ae274fea00036833b0816c97e79efb713e
Jose Medrano <jose.medrano@microsoft.com>
2018-01-24 22:00:55 +0100

    Fixed broken build

---
+++ HapSharp.Host.Core/MessageDelegates/MessageTemperatureDelegate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using HapSharp.MessageDelegates;
+
+namespace HapSharp.Accessories
+{
+	public abstract class MessageTemperatureDelegate : GetMessageDelegate
+	{
+		Random rnd = new Random (DateTime.Now.Millisecond);
+
+		protected MessageTemperatureDelegate (TemperatureAccessory accessory) : base (accessory)
+		{
+		}
+
+		public override int OnGetMessageReceived ()
+		{
+			var calculated = rnd.Next (20, 50);
+			Console.WriteLine ($"[Net][{Accessory.Name}][Get] {calculated}");
+			return calculated;
+		}
+
+		public override void OnIdentify ()
+		{
+			Console.WriteLine ($"[Net][{Accessory.Name}] Identified");
+		}
+	}
+}
