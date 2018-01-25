92d6400bdf40e58e5a2c1c19159e821539abe38c
Jose Medrano <jose.medrano@microsoft.com>
2018-01-25 03:50:16 +0100

    Adds a Broker project to serve local connections

---
+++ HapSharp.Host.Broker/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using uPLibrary.Networking.M2Mqtt;
+
+namespace HapSharp.Host.Broker
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("[Broker] Starting in localhost...");
+			// create and start broker
+			MqttBroker broker = new MqttBroker ();
+			broker.ClientConnected += (s) => {
+				Console.WriteLine ($"[Broker] Client connected: {s.ClientId}");
+			};
+
+			broker.ClientDisconnected += (s) => {
+				Console.WriteLine ($"[Broker] Client disconnected: {s.ClientId}");
+			};
+			broker.Start ();
+			//Once the broker is started, you applciaiton is free to do whatever it wants. 
+			Console.WriteLine ("[Broker] Started. Awaiting clients");
+			while (true) {
+
+			}
+		}
+	}
+}
