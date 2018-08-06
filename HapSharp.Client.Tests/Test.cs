f15bd5ea231a33cbe54814db28bc50a686afd341
Jose Medrano <jose.medrano@microsoft.com>
2018-08-06 10:30:26 +0200

    Reuses GetActualServices from HapClient

--- HapSharp.Client.Tests/Test.cs
+++ HapSharp.Client.Tests/Test.cs
@@ -10,30 +10,14 @@ namespace HapSharp.Client.Tests
 	[TestFixture ()]
 	public class Test
 	{
-		public async Task<IReadOnlyList<IZeroconfHost>> ProbeForNetworkPrinters ()
-		{
-			return await
-				ZeroconfResolver.ResolveAsync ("_hap._tcp.local.");
-		}
-
-		//_hap._tcp
-
-		public async Task EnumerateAllServicesFromAllHosts ()
-		{
-			ILookup<string, string> domains = await ZeroconfResolver.BrowseDomainsAsync ();
-			var responses = await ZeroconfResolver.ResolveAsync (domains.Select (g => g.Key));
-			foreach (var resp in responses)
-				Console.WriteLine (resp);
-		}
-
 		[Test ()]
-		public async void TestCase ()
+		public async Task GetActualServices ()
 		{
-			var lol = await ProbeForNetworkPrinters ();
-
-			//var ini = lol.FirstOrDefault (s => s.DisplayName (s => s.));
-			//System.Console.WriteLine ("");
-
+			var services = await HapClient.GetServices ();
+			foreach (var service in services) {
+				Console.WriteLine (service);
+			}
+			Assert.AreNotEqual (0, services.Count);
 		}
 	}
 }
